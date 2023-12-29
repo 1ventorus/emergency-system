a5255823ec1445b5db4c5e4c673f758b3da10317
1ventorus <113920033+1ventorus@users.noreply.github.com>
2023-12-29 23:55:33 +0100

    Update maj.py

--- test/sys_apps/maj.py
+++ test/sys_apps/maj.py
@@ -1,6 +1,6 @@
 import os
-import time
-import platform
+import ssl
+import socket
 import urllib.request
 
 def parentdir():
@@ -10,6 +10,17 @@ def parentdir():
     except Exception as e:
         print(f"Erreur lors du changement de répertoire : {e}")
 
+def check_internet_connection():
+    try:
+        socket.create_connection(("www.google.com", 443))
+        context = ssl.create_default_context()
+        with socket.create_connection(("www.google.com", 443)) as sock:
+            with context.wrap_socket(sock, server_hostname="www.google.com") as ssock:
+                return True, ssock.version()
+    except OSError:
+        return False, None
+connected, ssl_version = check_internet_connection()
+
 def fetch_file(url, filename):
     urllib.request.urlretrieve(url, filename)
 
@@ -17,7 +28,11 @@ def maj():
     parentdir()
     parentdir()
     os.remove("launcher.py")
+    os.remove("ES.py")
+    os.remove("ES_setup.py")
     fetch_file("https://raw.githubusercontent.com/1ventorus/emergency-system/main/launcher.py", "launcher.py")
+    fetch_file("https://raw.githubusercontent.com/1ventorus/emergency-system/main/test/ES.py", "ES.py")
+    fetch_file("https://raw.githubusercontent.com/1ventorus/emergency-system/main/ES_setup.py", "ES_setup.py")
     os.chdir("test")
     os.chdir("sys_apps")
     os.remove("cmd.py")
@@ -29,4 +44,7 @@ def maj():
     fetch_file("https://raw.githubusercontent.com/1ventorus/emergency-system/main/test/sys_apps/file_manager.py", "file_manager.py")
     fetch_file("https://raw.githubusercontent.com/1ventorus/emergency-system/main/test/sys_apps/maj.py", "maj.py")
 
-maj()
+if connected:
+    maj()
+else:
+    print("vous n'étes pas connecté")
